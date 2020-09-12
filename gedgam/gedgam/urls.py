bc8b20bfe5d3dd23e641e0ebafc563830de3cbc2
Emerson Bezerra <emersondevelops@gmail.com>
2020-09-12 00:08:39 -0300

    Listagem de games movida para a home.

--- gedgam/gedgam/urls.py
+++ gedgam/gedgam/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gerenciador/', include('gerenciador.urls'))
+    path('', include('gerenciador.urls'))
 ]

710ec43cd3d53911c7e4164fb4d6b47068e6f080
Emerson Bezerra <emersondevelops@gmail.com>
2020-09-11 22:28:26 -0300

    Migrations adicionadas.

--- gedgam/gerenciador/models.py
+++ gedgam/gerenciador/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    titulo = models.CharField(max_length=100)
+    ano_lancamento = models.IntegerField()
+    produtora = models.CharField(max_length=50)
+    PLATAFORMA = (
+        ('PC', 'PC'),
+        ('XB', 'Xbox One'),
+        ('PS', 'Play Station 4'),
+        ('NS', 'Nintendo Switch')
+    )
+    plataforma = models.CharField(max_length=20, choices=PLATAFORMA)
+    ZERADO = (
+        ('S', 'Sim'),
+        ('N', 'Não'),
+        ('O', 'Não se aplica'),
+    )
+    zerado = models.CharField(max_length=1, choices=ZERADO)
+    data_zeramento = models.DateField(null=True)
+    comentarios = models.TextField()
